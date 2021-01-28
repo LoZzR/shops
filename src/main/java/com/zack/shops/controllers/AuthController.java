ae3795829c69475fee2582892969d5c33ab1c7eb
LoZzR <zakariaelhichem@gmail.com>
2021-01-28 14:11:45 +0100

    Apply authorization access on Shop resources

--- src/main/java/com/zack/shops/controllers/AuthController.java
+++ src/main/java/com/zack/shops/controllers/AuthController.java
@@ -32,7 +32,7 @@ import com.zack.shops.repositories.UserRepository;
 import com.zack.shops.security.jwt.JwtUtils;
 import com.zack.shops.security.services.UserDetailsImpl;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
--- src/main/java/com/zack/shops/controllers/ShopController.java
+++ src/main/java/com/zack/shops/controllers/ShopController.java
@@ -3,6 +3,7 @@ package com.zack.shops.controllers;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,43 +20,50 @@ import com.zack.shops.services.ShopService;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/shops")
+@RequestMapping("/api/shops")
 public class ShopController {
 
 	@Autowired
 	private ShopService shopService;
 	
 	@GetMapping("")
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
 	public Set<Shop> getShops() {
-		throw new RuntimeException();
+		return this.shopService.getShops();
 	}
 	
 	@GetMapping("/liked")
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
 	public Set<Shop> getLikedShops() {
 		return this.shopService.getLikedShops();
 	}
 	
 	@PostMapping("/like/{idShop}")
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
 	public void likeShop(@PathVariable int idShop) {
 		this.shopService.likeShop(idShop);
 	}
 	
 	@PostMapping("")
+	@PreAuthorize("hasRole('ADMIN')")
 	public Shop addShop(@RequestBody Shop shop) {
 		return this.shopService.addShop(shop);
 	}
 	
 	@GetMapping("/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
 	public Shop getShop(@PathVariable int id) {
 		return this.shopService.getShopById(id);
 	}
 	
 	@PutMapping("")
+	@PreAuthorize("hasRole('ADMIN')")
 	public Shop editShop(@RequestBody Shop shop) {
 		return this.shopService.editShop(shop);
 	}
 	
 	@DeleteMapping("/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public void deleteShop(@PathVariable int id) {
 		this.shopService.deleteShop(id);
 	}
